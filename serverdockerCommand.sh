16385aec63454c9222266975d97603ecb338f749
shahp <priyansh.shah1997@gmail.com>
2020-03-21 23:35:38 +0530

    updated client

---
+++ serverdockerCommand.sh
@@ -0,0 +1,20 @@
+echo "\n\n\n this commanad will execute npm install:"
+npm install
+
+echo "\n\n\nCopy .env file:"
+file="./server/.env.docker"
+if [ -f "$file" ]
+then
+	echo "$file found."
+	cp $file ./.env
+	echo ".env created"
+else
+	echo "$file not found."
+	exit 1
+fi
+
+echo "\n\n\nRun migration:"
+adonis migration:run --force
+
+echo "\n\n\nStart node server:"
+adonis serve --dev --polling
